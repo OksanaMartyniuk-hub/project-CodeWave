c860538f30688ea2c741a214d5bbb932328e7edf
xenoterror <anohinnikita537@gmail.com>
2026-05-23 14:58:09 +0300

    added swiper into JS. lil fix in HTML. Mobile CSS.

--- src/js/feedback-section.js
+++ src/js/feedback-section.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import Raty from 'raty-js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const BASE_URL = 'https://deserts-store.b.goit.study/api/feedbacks/';
 
@@ -31,7 +36,7 @@ function createFeedbacks(responses) {
       <li class="swiper-slide">
       <div class="review-box">
         <div class="review-rating" data-rate="${response.rate}"></div>
-        <p class="review-desc">${response.description}</p>
+        <p class="review-desc">"${response.description}"</p>
         <p class="review-author">${response.author}</p>
         </div>
       </li>
@@ -62,7 +67,35 @@ function createStars() {
 
 async function init() {
   const responses = await getFeedbacksResponse();
+
   createFeedbacks(responses.feedbacks);
+
+  new Swiper('.review-swiper', {
+    modules: [Navigation, Pagination],
+    spaceBetween: 24,
+
+    pagination: {
+      el: '.review-pagination',
+      clickable: true,
+      dynamicBullets: true,
+      draggable: true,
+    },
+
+    navigation: {
+      prevEl: '.review-btn.prev',
+      nextEl: '.review-btn.next',
+    },
+
+    breakpoints: {
+      375: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
 }
 
 init();
